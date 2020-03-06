5009706e4d3790c4f5d3119d5300595bcf58bd98
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-06 15:59:32 +0530

    ADDED:Test case for returning sad as message

---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest
+{
+    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    String result;
+    // Test case for Given “I am in Sad Mood” message should return SAD
+    @Test
+    public void givenMessage_WhenProper_RespondAsSadMood()
+    {
+        result = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assert.assertEquals("SAD",result);
+    }
+}
