4234b88f7d19f0ba8083f46ae13e90af868fef03
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-06 16:57:10 +0530

    REFACTORED:Given input from constructor to the method to return message

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -3,20 +3,13 @@ import org.junit.Test;
 
 public class MoodAnalyserTest
 {
-    MoodAnalyser moodAnalyser = new MoodAnalyser();
+    MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
     String result;
-    // Test case for Given “I am in Sad Mood” message should return SAD
+    // Test case for Given “I am in Sad Mood” message through constructor should return SAD
     @Test
     public void givenMessage_WhenProper_RespondAsSadMood()
     {
-        result = moodAnalyser.analyseMood("I am in Sad Mood");
+        result = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",result);
-    }
-    // Test case for Given “I am in Any Mood” message should return Happy
-    @Test
-    public void givenMessage_WhenProper_RespondAsHappyMood()
-    {
-        result = moodAnalyser.analyseMood("I am in Any Mood");
-        Assert.assertEquals("HAPPY",result);
     }
 }
