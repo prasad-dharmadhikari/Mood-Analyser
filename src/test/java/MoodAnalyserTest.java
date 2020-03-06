972f5a788d119c330bf8009870562c6bb5047770
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-06 17:13:08 +0530

    REFACTORED:Added test case to pass input from constructor

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -3,13 +3,22 @@ import org.junit.Test;
 
 public class MoodAnalyserTest
 {
-    MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+    MoodAnalyser moodAnalyser;
     String result;
     // Test case for Given “I am in Sad Mood” message through constructor should return SAD
     @Test
     public void givenMessage_WhenProper_RespondAsSadMood()
     {
+        moodAnalyser = new MoodAnalyser("I am in Sad Mood");
         result = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD",result);
+    }
+    // Test case for Given “I am in Happy Mood” message through constructor should return HAPPY
+    @Test
+    public void givenMessage_WhenProper_RespondAsHappyMood()
+    {
+        moodAnalyser = new MoodAnalyser("I am in Happy Mood");
+        result = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY",result);
     }
 }
