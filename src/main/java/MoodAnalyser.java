dc7487dd1ab2d4b3a6e52c55b221bcbde88ebb85
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-06 15:54:55 +0530

    ADDED:Function for analysing mood

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,7 +1,16 @@
 public class MoodAnalyser
 {
-    public static void main(String[] args)
+    private String returnMessage;
+    public String analyseMood(String message)
     {
-        System.out.println("Welcome to mood analyser problem");
+        if (message.equals("I am in Sad Mood"))
+        {
+            returnMessage = "SAD";
+        }
+        else if (message.equals("I am in Any Mood"))
+        {
+            returnMessage = "HAPPY";
+        }
+        return  returnMessage;
     }
 }
