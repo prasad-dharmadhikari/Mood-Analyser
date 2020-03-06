5c41201d9664cda9dee4fbff6fccf8e2221b1ebd
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-06 16:52:41 +0530

    REFACTORED:Changed the nature of the function to accept no parameters and accept only input from constructor

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,16 +1,28 @@
 public class MoodAnalyser
 {
+    private String message;
     private String returnMessage;
+    //no-arg constructor
+    public MoodAnalyser()
+    {
+
+    }
+    //parametrized constructor
+    public MoodAnalyser(String message)
+    {
+        this.message = message;
+    }
     public String analyseMood(String message)
+    {
+        this.message = message;
+        return  analyseMood();
+    }
+    public String analyseMood()
     {
         if (message.equals("I am in Sad Mood"))
         {
-            returnMessage = "SAD";
+            returnMessage="SAD";
         }
-        else if (message.equals("I am in Any Mood"))
-        {
-            returnMessage = "HAPPY";
-        }
-        return  returnMessage;
+        return returnMessage;
     }
 }
