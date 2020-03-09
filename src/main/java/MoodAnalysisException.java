d72155e3aa41e5b41a758158f41ae74bc599b34c
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-09 15:47:15 +0530

    REFACTORED:Added enum named exception type to distinguish between type of exceptions

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,8 +1,14 @@
 
 public class MoodAnalysisException extends Exception
 {
-    public MoodAnalysisException(String message)
+    enum ExceptionType
+    {
+        ENTERED_NULL,ENTERED_EMPTY
+    }
+    ExceptionType type;
+    public MoodAnalysisException(ExceptionType type,String message)
     {
         super(message);
+        this.type = type;
     }
 }
