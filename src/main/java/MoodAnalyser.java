21239be10795b853d62bb76382b50ec49e9814e4
prasad-dharmadhikari <prasad.dharmadhikari4669@gmail.com>
2020-03-13 15:24:59 +0530

    REFACTORED:changed access modifiers of variables

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -2,8 +2,8 @@ import java.util.Objects;
 
 public class MoodAnalyser
 {
-    private String message;
-    private String returnMessage;
+    public String message;
+    public String returnMessage;
     //no-arg constructor
     public MoodAnalyser()
     {
